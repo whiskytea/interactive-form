694b991fcdebbb2774e660a838aae3fc8779fc44
Admiin <silntmind@gmail.com>
2021-07-09 18:55:08 -0700

    so many updates
    just gonna push all the updates through - I'll be better about smaller batch updates in the future

--- js/script.js
+++ js/script.js
@@ -58,11 +58,64 @@ activitiesBox.addEventListener('change', () =>{
     totalContainer.textContent = `Total: $${total}`;
     for (let childElement of activitiesBox.children){
         for (let labelElements of childElement.children){
-            console.log(labelElements);
             if (labelElements.checked){
-                total += labelElements.dataset.cost;
+                total += parseInt(labelElements.dataset.cost);
             }
         }
     }
     totalContainer.textContent = `Total: $${total}`;
+})
+
+//"Payment Info" section
+
+const paymentMethods = document.querySelector('#payment');
+const creditCard = document.querySelector('#credit-card');
+const paypal = document.querySelector('#paypal');
+const bitcoin = document.querySelector('#bitcoin');
+
+document.addEventListener('DOMContentLoaded', () => {
+    paymentMethods.value = "credit-card";
+    paypal.style.display = "none";
+    bitcoin.style.display = "none";
+})
+
+paymentMethods.addEventListener('change', () =>{
+    let paymentChoice = paymentMethods.value;
+    if (paymentChoice === 'credit-card'){
+        creditCard.style.display = "block";
+        paypal.style.display = "none";
+        bitcoin.style.display = "none";
+    }
+    else if (paymentChoice === 'paypal'){
+        creditCard.style.display = "none";
+        paypal.style.display = "block";
+        bitcoin.style.display = "none";
+    }
+    else if (paymentChoice === 'bitcoin'){
+        creditCard.style.display = "none";
+        paypal.style.display = "none";
+        bitcoin.style.display = "block";
+    }
+})
+
+// From Validation
+
+const form = document.querySelector('form');
+const nameField = document.querySelector('#name');
+const emailField = document.querySelector('#email');
+const verifyEmail = new RegExp("[a-zA-Z0-9._]+@[a-zA-Z0-9]+.(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)"); // RegExp for email validation is a modified version of the regexp found here: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#validation
+
+form.addEventListener('submit', (e) =>{
+    
+    if (nameField.value === ''){
+        nameField.classList.add("not-valid");
+        e.preventDefault();
+    }
+    if (!verifyEmail.test(emailField.value)){
+        console.log(verifyEmail.test(emailField.value))
+        e.preventDefault();
+        emailField.style.border = "1px solid red";
+    }
+    
+
 })
